5783aa7961037a2e1cc5c4e11ab357f8c096f5af
Aaron O'Leary <eeaol@leeds.ac.uk>
2015-04-28 10:28:24 +0100

    ignore readme

--- setup.py
+++ setup.py
@@ -3,10 +3,14 @@ import os
 
 
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    try:
+        content = open(os.path.join(os.path.dirname(__file__), fname)).read()
+    except IOError:
+        content = ''
+    return content
 
 setup(name='parallelprogress',
-      version='0.1',
+      version='0.1.1',
       description='Share a progress bar between parallel processes',
       long_description=read('README.md'),
       author="Aaron O'Leary",
